c97677052512e036fbb9e5bf014ebba64482a019
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-07 12:05:17 -0600

    js class notes, warming up for class. the foundations & cornerstone

---
+++ js/js-warmups.js
@@ -0,0 +1,20 @@
+"use strict";
+//01write a function that takes an input and returns the string "I have no idea what you
+//01just said";
+
+function functionName01(inputName01) {
+	return "I have no idead what you just said";
+}
+console.log(functionName01("hi"));
+
+//02write a function that takes in a number and returns that number times 2;
+//02so if you give it 2 it returns 4;
+//02if you give it 100 it returns 200;
+
+function functionName02(number02) {
+	return number02 * 2;
+}
+console.log(functionName02(2), 4);
+console.log(functionName02(100), 200);
+
+//03
