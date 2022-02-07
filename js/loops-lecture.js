9beafa4272c7b3fca363d66d97cfec1f66780cf4
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-07 12:06:22 -0600

    lecture notes JS loops, learning loops

---
+++ js/loops-lecture.js
@@ -0,0 +1,46 @@
+// var slices = 4;
+// /** while is a loop keyword**/while/**going to check if slices are greater than 0**/(slices > 0){/**the code block starts(everything in between), it executes if yes/true, the slices are greater than 0**/
+// 	console.log("I'll have a slice!");
+// 	slices = slices -1;/** reduces the slices by 1 based upon var slices = 4**/
+// 	console.log("Now there's " + slices + " left.");
+// 	/**the code block ends**/}
+// console.log("Aw, no more pizza :("); /** when it's finally false**/
+
+//getting rid of slices = would be an infinite loop making it force quit...
+
+// ================================================
+// var counter = 0;
+// while(counter <= 100){
+// 	console.log(counter);
+// 	//counter = counter -1;
+// 	counter++;
+// }
+//
+// var /**counter = conditional**/counter = 100;
+// while (counter >= 0/**as long as this is true,loop will continue**/){
+// 	console.log(counter);
+// 	counter--;
+// }
+// console.log("I'm out of the loop!");
+
+// var lives = 3; /** how many times you want this to happen**/
+// while(lives > 3){
+// 	console.log(lives);
+// 	lives--;
+// }
+// console.log("game over");
+// var counter = 1;
+// var number = 5;
+// while (counter <=5){
+// 	number = number * 2;
+// 	counter = counter + 1;
+// 	console.log(number);
+// }
+/**where you put your output -the console.log()- makes a difference
+ * in the loop {part of the loop} **/
+// var number = 5;
+// while (number < 200){
+// 	console.log(number);
+// 	number = number * 2;
+// }
+//-----------------------------------------------------------------------------;
