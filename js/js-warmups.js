3a100caab483ca181f7fef4b133701ca612aeda0
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-08 09:31:47 -0600

    warming up for class in js-warmups.js

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -2,19 +2,90 @@
 //01write a function that takes an input and returns the string "I have no idea what you
 //01just said";
 
-function functionName01(inputName01) {
-	return "I have no idead what you just said";
-}
-console.log(functionName01("hi"));
+// function functionName01(inputName01) {
+// 	return "I have no idead what you just said";
+// }
+// console.log(functionName01("hi"));
 
 //02write a function that takes in a number and returns that number times 2;
 //02so if you give it 2 it returns 4;
 //02if you give it 100 it returns 200;
 
-function functionName02(number02) {
-	return number02 * 2;
-}
-console.log(functionName02(2), 4);
-console.log(functionName02(100), 200);
+// function functionName02(number02) {
+// 	return number02 * 2;
+// }
+// console.log(functionName02(2), 4);
+// console.log(functionName02(100), 200);
 
-//03
+//08FEB2022
+//1.write a function that takes in a number and returns that number plus itself;
+
+// function fName(aNum) {
+// 	return aNum + aNum;
+// }
+// console.log(fName());
+
+
+//2.now write a function that takes in two numbers doubles each number, and then returns the sum of both numbers doubled;
+
+// function doubletheNums(num1, num2) {
+// 	var num1d = num1 * 2;
+// 	var num2d = num2 * 2;
+// 	return num1d + num2d;
+// }
+// console.log(doubletheNums(1,1))
+
+// instructor:
+// function sumOfDoubles (number1, number2) {
+// 	return (number1 * 2) + (number2 * 2);
+// }
+
+
+//3.write a function that takes in a number and squares it (that is, returns that number multiplied by itself;
+
+// function squared2(aNum) {
+// 	return aNum * aNum;
+// }
+//or type squared2(put the number) in the console on chrome.
+// console.log(squared2(10))
+
+//instructor:
+// function squareANumber(number){
+// 	return number * number;
+// }
+// ||orrrr
+// function squareAnumber(number){
+// 	return number**2;
+// }
+
+//4. write a function that takes in a string and returns its length
+// function strLength (strL) {
+// // 	return strL.length;
+// // }
+// // console.log(strLength("love"));
+
+//instructor:
+// function stringLength(string){
+// 	return string.length;
+// }
+
+//5. write a function that takes in two strings and returns the sum of their lengths
+// function twoSTRSL(str1, str2) {
+// 	return str1.length + str2.length;
+// }
+// console.log(twoSTRSL("love","love"))
+
+//instructor:
+// function sumOfStringLengths(string1, string2){
+// 	return string1.length + string2.length;
+// }
+
+//6.write a function that takes in a number and returns the string "even" or "odd" depending on wether the number is even or odd.
+
+// function oddOreven(number){
+// 	if(number % 2 === 0){
+// 		return "even";
+// 	} else {
+// 		return "odd";
+// 	}
+// }
