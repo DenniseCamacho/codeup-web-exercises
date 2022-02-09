1ab0fc9c6f0b149bea198cf1eeb3b08b6154ee91
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-09 08:55:01 -0600

    iterating arrays exercises, not finished yet, before class review

---
+++ js/iterating.js
@@ -0,0 +1,67 @@
+(function(){
+	"use strict";
+
+	/**
+	 * TODO:
+	 * Create an array of 4 people's names and store it in a variable called
+	 * 'names'.
+	 */
+	// var names = ["Spike", "Rain", "Coco", "Chiquita"];
+
+	/**
+	 * TODO:
+	 * Create a log statement that will log the number of elements in the names
+	 * array.
+	 */
+	 // for (var i = 0; i < names.length; i++) {
+		//  console.log(i);
+	 // }
+	/**
+	 * TODO:
+	 * Create log statements that will print each of the names individually by
+	 * accessing each element's index.
+	 */
+	// for (var i = 0; i < names.length; i++) {
+	// 	console.log(i + " is " + names[i]);
+	// }
+	/**
+	 * TODO:
+	 * Write some code that uses a for loop to log every item in the names
+	 * array.
+	 */
+	// for (var i = 0; i < names.length; i++) {
+	// 	console.log([i]);
+	// }
+	/**
+	 * TODO:
+	 * Refactor your above code to use a `forEach` loop
+	 */
+// names.forEach(function (name,index){
+// 	console.log("Name " + (index + 1) + " is " + name);
+// });
+	/**
+	 * TODO:
+	 * Create the following three functions, each will accept an array and
+	 * return an an element from it
+	 * - first: returns the first item in the array
+	 * - second: returns the second item in the array
+	 * - last: returns the last item in the array
+	 *
+	 * Example:
+	 *  > first([1, 2, 3, 4, 5]) // returns 1
+	 *  > second([1, 2, 3, 4, 5]) // returns 2
+	 *  > last([1, 2, 3, 4, 5]) // return 5
+	 */
+	var nums = ["1", "2", "3", "4", "5"];
+
+	// for (var i = 0;  i < nums.length; i++) {
+	// 	console.log("index " + (i + 1) + " = " + nums[i])
+	// }
+		for (var i = 0;  i < nums.length; i++) {
+			console.log("index " + (i + 1) + " = " + nums[i]);
+		}
+
+		console.log(nums[1] " is first");
+		console.log(nums[5] " is last");
+		console.log(nums[2] " is second");
+})();
