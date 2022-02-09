9d1f1559d72e6ab6cb17bcdd95fd1ca0a65b9ce2
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-09 08:51:49 -0600

    adding files where I practice things with tutors or at home.

---
+++ js/practiceFEB2022.js
@@ -0,0 +1,37 @@
+// function showMultiplicationTable (input) {
+// 	for (var i = 1; i <= 10; i++) {
+// 		console.log(input + "x" + i + "=" + (input*i))
+// 	}
+// }
+// showMultiplicationTable(7);
+
+// for (var i = 1; i <= 10; i++) {
+// 	var randomNum = Math.floor(Math.random() * 181) + 20;
+// 	if (randomNum % 2 == 0) {
+// 		console.log(randomNum + " is even.");
+// 	} else {
+// 		console.log(randomNum + " is odd.")
+// 	}
+// }
+
+// for (var i = 1; i < 10; i++) {
+// 	console.log(i.toString().repeat(i));
+// }
+//
+
+// for (var i = 100; i >= 5; i -= 5) {
+// 	console.log(i);
+// }
+
+// var i = 2;
+// while (i <= 65536) {
+// 	console.log(i);
+// 	i = i * 2;
+// }
+
+// var sum = 0;
+// function sumOdd(num) {
+// 	for (var i = 1; i <= num; i += 2) {
+// 		sum = sum + i;
+// 	}
+// }
