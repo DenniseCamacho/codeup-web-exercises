b6ca45a2cec81a7e37c6b7421dcce6ebe3a623ea
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-09 22:23:44 -0600

    practicing for exam tomorrow Lw

--- js/practiceFEB2022.js
+++ js/practiceFEB2022.js
@@ -1,37 +1,64 @@
-// function showMultiplicationTable (input) {
-// 	for (var i = 1; i <= 10; i++) {
-// 		console.log(input + "x" + i + "=" + (input*i))
-// 	}
-// }
-// showMultiplicationTable(7);
-
-// for (var i = 1; i <= 10; i++) {
-// 	var randomNum = Math.floor(Math.random() * 181) + 20;
-// 	if (randomNum % 2 == 0) {
-// 		console.log(randomNum + " is even.");
-// 	} else {
-// 		console.log(randomNum + " is odd.")
-// 	}
-// }
-
-// for (var i = 1; i < 10; i++) {
-// 	console.log(i.toString().repeat(i));
-// }
+// // function showMultiplicationTable (input) {
+// // 	for (var i = 1; i <= 10; i++) {
+// // 		console.log(input + "x" + i + "=" + (input*i))
+// // 	}
+// // }
+// // showMultiplicationTable(7);
 //
+// // for (var i = 1; i <= 10; i++) {
+// // 	var randomNum = Math.floor(Math.random() * 181) + 20;
+// // 	if (randomNum % 2 == 0) {
+// // 		console.log(randomNum + " is even.");
+// // 	} else {
+// // 		console.log(randomNum + " is odd.")
+// // 	}
+// // }
+//
+// // for (var i = 1; i < 10; i++) {
+// // 	console.log(i.toString().repeat(i));
+// // }
+// //
+//
+// // for (var i = 100; i >= 5; i -= 5) {
+// // 	console.log(i);
+// // }
+//
+// // var i = 2;
+// // while (i <= 65536) {
+// // 	console.log(i);
+// // 	i = i * 2;
+// // }
+//
+// // var sum = 0;
+// // function sumOdd(num) {
+// // 	for (var i = 1; i <= num; i += 2) {
+// // 		sum = sum + i;
+// // 	}
+// // }
+//
+// //
+// //
+// // var navigation = ["Home", "About", "Contact", "Location"];
+// //
+//
+//
+//
+// (function() {
+//
+// 	"use strict";
+//
+// 	/**
+// 	 * Tests for the following functions. See README.md for more detail.
+// 	 * isTrue
+// 	 * isFalse
+// 	 * not
+// 	 * addOne
+// 	 * isEven
+// 	 * isIdentical
+// 	 * isEqual
+// 	 * or
+// 	 * and
+// 	 * concat
+// 	 */
 
-// for (var i = 100; i >= 5; i -= 5) {
-// 	console.log(i);
-// }
 
-// var i = 2;
-// while (i <= 65536) {
-// 	console.log(i);
-// 	i = i * 2;
-// }
-
-// var sum = 0;
-// function sumOdd(num) {
-// 	for (var i = 1; i <= num; i += 2) {
-// 		sum = sum + i;
-// 	}
-// }
