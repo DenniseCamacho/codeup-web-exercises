482576ac734fe7550d57a2eb2387f2e2393a532d
Dennise Camacho <dennise.e.camacho@gmail.com>
2022-02-11 15:32:52 -0600

    objects class exercise

--- js/practiceFEB2022.js
+++ js/practiceFEB2022.js
@@ -1,64 +1,130 @@
-// // function showMultiplicationTable (input) {
-// // 	for (var i = 1; i <= 10; i++) {
-// // 		console.log(input + "x" + i + "=" + (input*i))
-// // 	}
-// // }
-// // showMultiplicationTable(7);
-//
-// // for (var i = 1; i <= 10; i++) {
-// // 	var randomNum = Math.floor(Math.random() * 181) + 20;
-// // 	if (randomNum % 2 == 0) {
-// // 		console.log(randomNum + " is even.");
-// // 	} else {
-// // 		console.log(randomNum + " is odd.")
-// // 	}
-// // }
-//
-// // for (var i = 1; i < 10; i++) {
-// // 	console.log(i.toString().repeat(i));
-// // }
+// // // function showMultiplicationTable (input) {
+// // // 	for (var i = 1; i <= 10; i++) {
+// // // 		console.log(input + "x" + i + "=" + (input*i))
+// // // 	}
+// // // }
+// // // showMultiplicationTable(7);
 // //
-//
-// // for (var i = 100; i >= 5; i -= 5) {
-// // 	console.log(i);
-// // }
-//
-// // var i = 2;
-// // while (i <= 65536) {
-// // 	console.log(i);
-// // 	i = i * 2;
-// // }
-//
-// // var sum = 0;
-// // function sumOdd(num) {
-// // 	for (var i = 1; i <= num; i += 2) {
-// // 		sum = sum + i;
-// // 	}
-// // }
-//
+// // // for (var i = 1; i <= 10; i++) {
+// // // 	var randomNum = Math.floor(Math.random() * 181) + 20;
+// // // 	if (randomNum % 2 == 0) {
+// // // 		console.log(randomNum + " is even.");
+// // // 	} else {
+// // // 		console.log(randomNum + " is odd.")
+// // // 	}
+// // // }
+// //
+// // // for (var i = 1; i < 10; i++) {
+// // // 	console.log(i.toString().repeat(i));
+// // // }
+// // //
+// //
+// // // for (var i = 100; i >= 5; i -= 5) {
+// // // 	console.log(i);
+// // // }
+// //
+// // // var i = 2;
+// // // while (i <= 65536) {
+// // // 	console.log(i);
+// // // 	i = i * 2;
+// // // }
+// //
+// // // var sum = 0;
+// // // function sumOdd(num) {
+// // // 	for (var i = 1; i <= num; i += 2) {
+// // // 		sum = sum + i;
+// // // 	}
+// // // }
+// //
+// // //
+// // //
+// // // var navigation = ["Home", "About", "Contact", "Location"];
+// // //
 // //
 // //
-// // var navigation = ["Home", "About", "Contact", "Location"];
 // //
+// // (function() {
+// //
+// // 	"use strict";
+// //
+// // 	/**
+// // 	 * Tests for the following functions. See README.md for more detail.
+// // 	 * isTrue
+// // 	 * isFalse
+// // 	 * not
+// // 	 * addOne
+// // 	 * isEven
+// // 	 * isIdentical
+// // 	 * isEqual
+// // 	 * or
+// // 	 * and
+// // 	 * concat
+// // 	 */
 //
 //
+// // var pet = {};
+// // var pet = {givenName: "Freckles", ageInYears: 4, species: "Dog"};
+// // // pet.ageInYears = 5; // will change 4 to 5 ... you can put objects in arrays.
+// // // console.log(pet)
+// // pet.givenName = "";
+// // pet.ageInYears = 4;
+// // pet.species = "Dog";
 //
-// (function() {
+// //video game, you have a game object, you lives are set to three, a function that will kill character, you will have to update the number of lives to update the remaining lives.
+// // /**1.**/ var game = {}; //more current way of defining objects. var dog = object(); isnt relevent.
+// // /**2.**/var game = {lives: 3};
+// // game.lives -= 1;
+// // console.log(game);
 //
-// 	"use strict";
+// // // v--time saving way,
+// // var pet = {
+// // 	givenName: "Freckles",
+// // 	ageInYears: 4,
+// // 	species: "Dog"
+// // }
+// //^time saving way of writting an object instead of var pet ={}
+// // pet.givenName = "";
+// // pet.species.... ect.
+// /**
+// var game = {
+// 	numberOfLives: 3
+// }
+// console.log(game.numberOfLives); **/
 //
-// 	/**
-// 	 * Tests for the following functions. See README.md for more detail.
-// 	 * isTrue
-// 	 * isFalse
-// 	 * not
-// 	 * addOne
-// 	 * isEven
-// 	 * isIdentical
-// 	 * isEqual
-// 	 * or
-// 	 * and
-// 	 * concat
-// 	 */
-
+// // var pet = {
+// // 	typeOfspecies: "Dog"
+// // }
+// // var pet1 = {
+// // 	typeOfspecies: "Cat"
+// // }
+// // var pet2 = {
+// // 	typeOfspecies: "Mouse"
+// // }
+// // //indexof finds the index of the value not the item of the index , when you use [0] it finds the item,
+// // //indexOf returns -1  when the value is not in the index.
+// // var pets = [pet, pet1, pet2];
+// // console.log(pets[0].typeOfspecies);
+//
+// var pet = {
+// 	givenName: "Spot",
+// 	ageInYears: 4,
+// 	species: "Dog"
+// }
+// var pet2 = {
+// 	givenName: "Spot2",
+// 	alternativeNames: ["Buddy", "Rex", "Max"]
+// 	ageInYears: 4,
+// 	species: "Dog"
+// }
+// var pet3 = {
+// 	givenName: "Spot3",
+// 	ageInYears: 4,
+// 	species: "Dog"
+// }
+// console.log(pet2.givenName);
+// console.logg(pet2.alternativeNames)
+// console.log(pet2["givenName"]);
+//
+// //elements in an array = the values in the array
+// //alternativeNames: ["buddy", "max", "ruby"]
 
